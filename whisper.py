ccb319dfe721e95978a842f2d2e189cfb4466f8b
TERAKOSHITAISUKE <denpsyadvance@gmail.com>
2023-12-06 12:49:59 +0900

    1st

---
+++ whisper.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import openai
+import urllib
+import re
+import datetime
+
+# 音声ファイルを文字起こしする関数
+def transcribe(audio_filepath, prompt: str) -> str:
+    transcript = openai.Audio.transcribe(
+        file=open(audio_filepath, "rb"),
+        model="whisper-1",
+        prompt=prompt,
+    )
+    return transcript["text"]
+
+# 句読点で改行する関数
+def split_text_by_punctuation(text: str) -> str:
+    return re.sub(r'([。])', r'\1\n', text)
+
+# OpenAIのChatGPTを使用してテキストを要約する関数
+def summarize_text_with_chatgpt(text: str) -> str:
+    chat_model = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[
+            {"role": "system", "content": "You use only Japanese. So, you must response by Japanese."},
+            {"role": "user", "content": f"Please summarize the following content: {text}"},
+            {"role": "user", "content": "Total words is 1000 words."},
+            {"role": "user", "content": "Please insert a summarized title at the beginning."},
+        ],
+    )
+    return chat_model['choices'][0]['message']['content']
+
+# StreamlitのUIを定義
+st.title('音声ファイルの文字起こしと要約')
+uploaded_file = st.file_uploader("音声ファイルをアップロードしてください", type=['m4a'])
+
+if uploaded_file is not None:
+    # 音声ファイルを文字起こし
+    prompt = "日本語の音声を文字起こししてください。"
+    transcript = transcribe(uploaded_file, prompt)
+
+    # 句読点で改行
+    split_transcript = split_text_by_punctuation(transcript)
+
+    # 要約
+    summary = summarize_text_with_chatgpt(split_transcript)
+
+    # 結果を表示
+    st.write('文字起こし結果:')
+    st.write(transcript)
+    st.write('要約結果:')
+    st.write(summary)
